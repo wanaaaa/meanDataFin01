742a2250839309ee9c3e50d30185756f7636802b
wanaaaa <wanaaaa@gmail.com>
2015-12-22 18:31:32 -0500

    bar

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,63 @@
+//////////////////////////// Graph Start
+      //Width and height
+var w = 600;
+var h = 250;
+
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+        11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+
+var xScale = d3.scale.ordinal()
+        .domain(d3.range(dataset.length))
+        .rangeRoundBands([0, w], 0.05);
+var yScale = d3.scale.linear()
+        .domain([0, d3.max(dataset)])
+        .range([0, h]);
+
+//Create SVG element
+var svg = d3.select("#ggraph")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+//Create bars
+svg.selectAll("rect")
+   .data(dataset)
+   .enter()
+   .append("rect")
+   .attr("x", function(d, i) {
+      return xScale(i);
+   })
+   .attr("y", function(d) {
+      return h - yScale(d);
+   })
+   .attr("width", xScale.rangeBand())
+   .attr("height", function(d) {
+      return yScale(d);
+   })
+   .attr("fill", function(d) {
+    return "rgb(0, 0, " + (d * 10) + ")";
+   });
+//Create labels
+svg.selectAll("text")
+   .data(dataset)
+   .enter()
+   .append("text")
+   .text(function(d) {
+      return d;
+   })
+   .attr("text-anchor", "middle")
+   .attr("x", function(d, i) {
+      return xScale(i) + xScale.rangeBand() / 2;
+   })
+   .attr("y", function(d) {
+      return h - yScale(d) + 14;
+   })
+   .attr("font-family", "sans-serif")
+   .attr("font-size", "11px")
+   .attr("fill", "white");
+
+
+//////////////////////////// Graph end
+
 var app = angular.module('myApp', []);
 var jobbToSave;
 var testVar = "testest";
@@ -128,31 +188,7 @@ function createMarker(results) {
 
 // Google Map ends
 
-//////////////////////////// Graph Start
-var w = 500;
-var h = 50;
 
-//Data
-var dataset = [ 5, 10, 15, 20, 25 ];
-
-//Create SVG element
-var svg = d3.select("#graph")
-      .append("svg")
-      .attr("width", 500)
-      .attr("height", 50);
-var circles = svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle");
-circles.attr("cx", function(d, i) {
-      return (i * 50) + 25;
-    })
-     .attr("cy", h/2)
-     .attr("r", function(d) {
-      return d;
-     });
-
-//////////////////////////// Graph end
 
 $(function(){
 	$('div').on("click",".jobb",  function(){
