6d371c24ff72b41c559188189f07a93718959d7b
wanaaaa <wanaaaa@gmail.com>
2015-12-22 15:58:21 -0500

    grahp

--- public/js/app.js
+++ public/js/app.js
@@ -128,6 +128,31 @@ function createMarker(results) {
 
 // Google Map ends
 
+//////////////////////////// Graph Start
+var w = 500;
+var h = 50;
+
+//Data
+var dataset = [ 5, 10, 15, 20, 25 ];
+
+//Create SVG element
+var svg = d3.select("#graph")
+      .append("svg")
+      .attr("width", 500)
+      .attr("height", 50);
+var circles = svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle");
+circles.attr("cx", function(d, i) {
+      return (i * 50) + 25;
+    })
+     .attr("cy", h/2)
+     .attr("r", function(d) {
+      return d;
+     });
+
+//////////////////////////// Graph end
 
 $(function(){
 	$('div').on("click",".jobb",  function(){
